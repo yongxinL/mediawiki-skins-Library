ca07f8ce38890a1a6122f87e8b83610dfaa4e573
yongxinL <yongxinL@outlook.com>
2020-06-20 13:30:03 +1000

    Change fonts and layout for Skins

--- resources/skins.library.js/library.js
+++ resources/skins.library.js/library.js
@@ -5,7 +5,7 @@ $(function(){
 		var targ = $('.mw-sidebar .nav-link.active').attr('href');
 		$(targ).addClass('show');
 
-		if(window.matchMedia('(min-width: 1200px)').matches) {
+		if(window.matchMedia('(min-width: 1600px)').matches) {
 			$('.mw-sidebar-aside').addClass('show');
 		}
 
@@ -43,10 +43,28 @@ $(function(){
 		$('body').toggleClass('mw-sidebar-show');
 	});
 
-	// Shows header dropdown while hiding others
-	$('.mw-header .dropdown > a').on('click', function(e) {
+	// Shows topbar dropdown while hiding others
+	$('.mw-topbar .dropdown > a').on('click', function(e) {
 		e.preventDefault();
 		$(this).parent().toggleClass('show');
 		$(this).parent().siblings().removeClass('show');
 	});
+	
+	if (!window.xMode) {
+		var node = document.createElement('div');
+		node.className = 'to-top';
+		document.body.appendChild(node);
+		node.addEventListener('mousedown', function() {
+			this.classList.add('active');
+			$('html, body').stop().animate({
+				scrollTop: 0
+			}, 500, 'swing', function() {
+				this.classList.remove('active')
+			}.bind(this))
+		});
+		document.addEventListener('scroll', function() {
+			if (window.scrollY > window.innerHeight) node.classList.add('show');
+			else node.classList.remove('show')
+		})
+	}
 });
