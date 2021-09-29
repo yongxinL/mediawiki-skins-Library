746ce58758c0697655b3c9f9bf416f047bdfaf3b
yongxinL <yongxinL@outlook.com>
2021-09-29 11:51:36 +1000

    - Layout updates (ver 2.0.1)

--- resources/skins.library.js
+++ resources/skins.library.js
@@ -11,9 +11,9 @@
     var toc = document.getElementById("toc");
     if (typeof toc != "undefined" && toc != null) {
         var a = document.getElementById("contentAside");
-        a.classList.add('d-lg-block');
+        a.classList.add("d-lg-block");
 
-        toc.classList.add('card', 'sticky-top');
+        toc.classList.add("card", "sticky-top");
         var u = document.querySelectorAll("#toc ul");
         for (var i = 0; i < u.length; i++) {
             u[i].classList.add("nav");
@@ -28,14 +28,13 @@
             u[i].classList.add("nav-item");
         }
         a.appendChild(toc);
-    
+
         // var spyElem = document.getElementById("mw-content-text");
-        var spyElem = document.getElementsByTagName("body")[0]
+        var spyElem = document.getElementsByTagName("body")[0];
         spyElem.setAttribute("data-bs-spy", "scroll");
         spyElem.setAttribute("data-bs-target", "ul.scrollspy");
         spyElem.setAttribute("data-bs-offset", "0");
         spyElem.setAttribute("tabindex", "0");
-
     }
 
     // returns a function, that, as long as it continues to be invoked, will not
@@ -65,31 +64,20 @@
     var setViewport = function () {
         v = window.innerWidth || document.documentElement.clientWidth;
         b = document.getElementsByTagName("body")[0];
+        s = document.querySelector(".sidebar-container");
+        m = document.querySelector(".minibar-container");
         if (v >= 1400) {
             b.classList.add("sideshow");
-            document
-                .querySelector(".sidebar-container")
-                .classList.remove("drawer");
-            document
-                .querySelector(".minibar-container")
-                .classList.add("drawer");
+            s.classList.remove("drawer");
+           // m.classList.add("drawer");
         } else if (v < 1400 && v >= 992) {
             b.classList.remove("sideshow");
             b.classList.add("minishow");
-            document
-                .querySelector(".sidebar-container")
-                .classList.add("drawer");
-            document
-                .querySelector(".minibar-container")
-                .classList.remove("drawer");
+            s.classList.add("drawer");
+            // m.classList.remove("drawer");
         } else {
-            b.classList.remove("minishow");
-            document
-                .querySelector(".sidebar-container")
-                .classList.add("drawer");
-            document
-                .querySelector(".minibar-container")
-                .classList.add("drawer");
+            b.classList.remove("sideshow");
+            s.classList.add("drawer");
         }
     };
     // initial document by current viewport
@@ -99,82 +87,50 @@
         setViewport();
     });
 
-    // update classList when clicking toggler button
-    // by Vanilla JS
     document.addEventListener("DOMContentLoaded", function () {
-        // triggered when window scrollUp & Down
         var pos = 0;
-        var c = document.getElementById("commander").offsetTop;
-        window.addEventListener(
-            "scroll",
-            debounce(function () {
-                // var y = window.scrollY;
-                if (window.scrollY < pos) {
-                    document.getElementById("masthead").classList.add("show");
-                } else {
-                    document
-                        .getElementById("masthead")
-                        .classList.remove("show");
-                }
-                pos = window.scrollY;
-                if (window.scrollY > c) {
-                    document
-                        .getElementById("commander")
-                        .classList.add("sticky-top");
-                } else {
-                    document
-                        .getElementById("commander")
-                        .classList.remove("sticky-top");
-                }
-            }, 10)
-        );
-
-        // open searchForm when clicking
-        document
-            .getElementById("search")
-            .addEventListener("click", function () {
-                document.getElementById("masthead").classList.add("searchshow");
-            });
-
-        // close searchForm when clicking
-        document
-            .getElementById("searchclose")
-            .addEventListener("click", function () {
-                document
-                    .getElementById("masthead")
-                    .classList.remove("searchshow");
-            });
+        var m = document.getElementById("masthead");
+        if (typeof m != "undefined" && m != null) {
+            // show (hide) masthead when window scrollUp & Down
+            window.addEventListener(
+                "scroll",
+                debounce(function () {
+                    if (window.scrollY < pos) {
+                        m.classList.add("show");
+                    } else {
+                        m.classList.remove("show");
+                    }
+                    pos = window.scrollY;
+                }, 10)
+            );
+            // open (close) searchForm when clicking
+            document
+                .getElementById("search")
+                .addEventListener("click", function () {
+                    m.classList.add("searchshow");
+                });
+            document
+                .getElementById("searchclose")
+                .addEventListener("click", function () {
+                    m.classList.remove("searchshow");
+                });
+        }
 
         // open / close sidebar when clicking
         var t = document.getElementsByClassName("sidebar-toggler");
-        var b = document.getElementsByTagName("body")[0];
         var v = window.innerWidth || document.documentElement.clientWidth;
+        var b = document.getElementsByTagName("body")[0];
 
         for (var i = 0; i < t.length; i++) {
             t[i].addEventListener("click", function () {
                 if (b.classList.contains("sideshow")) {
+                    b.classList.add("minishow");
                     b.classList.remove("sideshow");
                 } else {
+                    b.classList.remove("minishow");
                     b.classList.add("sideshow");
                 }
             });
-
-            /* 					
-								
-                s.classList.contains('d-none')) {
-                s.classList.remove('d-none', 'animate__bounceInLeft');
-                s.classList.add('animate__bounceOutRight');
-                if (v > 768) {
-                    document.querySelector(".minibar").classList.remove('d-none');
-                }
-            } else {
-                s.classList.remove('animate__bounceOutRight');
-                s.classList.add('d-none', 'animate__bounceInLeft');
-                if (v > 768) {
-                    document.querySelector(".minibar").classList.add('d-none');
-                }
-            }
-        }); */
         }
     });
 })();
